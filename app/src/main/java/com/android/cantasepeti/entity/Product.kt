e4dd1639577c91a8073ac4bfd789e00950364622
Sahranur Er <sahranurerr@gmail.com>
2021-10-19 19:20:24 +0300

    Product listeleme işlemleri için Adapter ve Product sınıfı oluşturuldu.Databinding eklendi,Home fragment tasarımı düzenlendi

---
+++ app/src/main/java/com/android/cantasepeti/entity/Product.kt
@@ -0,0 +1,9 @@
+package com.android.cantasepeti.entity
+
+import java.io.Serializable
+
+data class Product(
+    val image:String? = null,
+    val name:String? = null
+
+):Serializable
--- app/src/main/java/com/android/cantasepeti/view/tabs/HomeFragment.kt
+++ app/src/main/java/com/android/cantasepeti/view/tabs/HomeFragment.kt
@@ -1,11 +1,14 @@
 package com.android.cantasepeti.view.tabs
 
 import android.os.Bundle
+import android.os.Handler
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ImageView
 import androidx.navigation.findNavController
+import com.android.cantasepeti.R
 import com.android.cantasepeti.databinding.FragmentHomeBinding
 import com.android.cantasepeti.view.MainActivity
 
@@ -17,12 +20,38 @@ class HomeFragment : Fragment() {
         binding = FragmentHomeBinding.inflate(inflater, container, false)
         (requireActivity() as MainActivity).showBottomNavigationBar()
 
+        binding?.buttonNext?.setOnClickListener {
+            binding?.imageViewCard?.setImageResource(R.drawable.image_two)
+        }
+         startSlider()
+
+
         binding?.btnMove?.setOnClickListener {
             val action = HomeFragmentDirections.actionHomeFragmentToDetailFragment()
             it.findNavController().navigate(action)
         }
 
+        val reycler_view = binding?.listRecyclerView
+
         return binding?.root
+    }
+    private fun startSlider() {
+        val loopImages = listOf(R.drawable.imagecard, R.drawable.image_two)
+        Handler().apply {
+            val runnable = object : Runnable {
+                var index = 0
+                var imageView = binding?.imageViewCard
+                override fun run() {
+                    imageView?.setImageResource(loopImages[index])
+                    index++
+                    if (index > loopImages.size - 1) {
+                        index = 0
+                    }
+                    postDelayed(this, 1000)
+                }
+            }
+            postDelayed(runnable, 1000)
+        }
     }
 
 }
